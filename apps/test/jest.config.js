7bcb51f3c6c044d12a558c15b6ddd4b8d2af5514
Rebecca Frances <rfrances@werfen.com>
2020-10-19 10:24:23 +0200

    SYSTEM change name

--- apps/test/jest.config.js
+++ apps/test/jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  displayName: 'werfen',
+  displayName: 'test',
   preset: '../../jest.preset.js',
   transform: {
     '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
@@ -9,5 +9,5 @@ module.exports = {
     ],
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: '../../coverage/apps/werfen',
+  coverageDirectory: '../../coverage/apps/test',
 };
--- apps/test/src/app/app.spec.tsx
+++ apps/test/src/app/app.spec.tsx
@@ -13,6 +13,6 @@ describe('App', () => {
   it('should have a greeting as the title', () => {
     const { getByText } = render(<App />);
 
-    expect(getByText('Welcome to werfen!')).toBeTruthy();
+    expect(getByText('Welcome to test!')).toBeTruthy();
   });
 });
